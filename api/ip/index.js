7422b1ec68a18c5b73158cfae83b3e61a9eca07d
etherio97 <ethereal97@gmail.com>
2022-04-02 13:06:11 +0630

    fix getting ip

--- api/ip/index.js
+++ api/ip/index.js
@@ -1,6 +1,6 @@
 const app = require('../../src/core/app');
 
-app.get((req, res) => {
+app.get('*', (req, res) => {
   let ip = (req.headers['x-forwarded-for'] || '').split(',').pop().trim() ||
     req.socket.remoteAddress;
   res.json({ ip });
