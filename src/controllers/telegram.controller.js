043ccb3d1980bfad74e786fb6b8bf16296e55095
etherio97 <ethereal97@gmail.com>
2022-04-30 19:45:10 +0630

    x

--- src/controllers/telegram.controller.js
+++ src/controllers/telegram.controller.js
@@ -30,10 +30,17 @@ class TelegramController {
   }
 
   async onMessage(text) {
-    let words = await supabase
-      .post('/rest/v1/rpc/find_word', { w: text.trim() })
-      .catch((e) => []);
-
+    let word = text.trim();
+    let words = await this._findWord(word);
+    if (words.length) {
+      this.response.generateResponse(words);
+    } else {
+      await this.findSimilarWord(word);
+    }
+  }
+  
+  async findSimilarWord(word) {
+    let words = await this._similarWord(word);
     if (words.length) {
       this.response.generateResponse(words);
     } else {
@@ -43,6 +50,14 @@ class TelegramController {
 
   isBotCommand(entities) {
     return !!entities.find((v) => v.type == 'bot_command');
+  }
+  
+  _findWord(w) {
+    return supabase.post('/rest/v1/rpc/find_word', { w }).catch((e) => []);
+  }
+  
+  _similarWord(w, x = 5) {
+    return supabase.post('/rest/v1/rpc/similar_word', { w, x }).catch((e) => []);
   }
 }
 
