e5cc1c7827a5ea7fd298cd876bec8d05168f962e
etherio97 <ethereal97@gmail.com>
2022-05-02 19:15:10 +0630

    nn

--- api/weather/index.js
+++ api/weather/index.js
@@ -3,7 +3,7 @@ import weather from '../../src/app/weather';
 
 const router = Router();
 
-router.get('/weather/search', async (req, res) => {
+router.get('/search', async (req, res) => {
   let { q } = req.query;
   if (!q) {
     return res.status(400).json({ error: 'required parameter: q' });
@@ -12,13 +12,13 @@ router.get('/weather/search', async (req, res) => {
   res.json(results);
 });
 
-router.get('/weather/:geoid', async (req, res) => {
+router.get('/:geoid', async (req, res) => {
   let { geoid } = req.params;
   let results = await weather.getLocation(geoid);
   res.json(results);
 });
 
-router.get('/weather/:geoid/:year/:month/:date', async (req, res) => {
+router.get('/:geoid/:year/:month/:date', async (req, res) => {
   let { geoid, year, month, date } = req.params;
   let results = await weather.getLocationDay(geoid, [year, month, date]);
   res.json(results);
