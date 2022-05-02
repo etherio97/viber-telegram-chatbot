fb5d2fe87debe4060726a5ad7de0120826df4e22
Shin Maung Maung <shinmaungmaung@mytel.com.mm>
2022-05-02 08:54:16 +0630

    a

--- src/controllers/telegram.controller.js
+++ src/controllers/telegram.controller.js
@@ -1,3 +1,4 @@
+import Fuse from 'fuse.js';
 import supabase from '../app/supabase';
 import { TelegramResponse } from '../app/TelegramResponse';
 
@@ -31,26 +32,46 @@ class TelegramController {
 
   async onMessage(text) {
     let word = text.trim();
-    let a = await this._findWord(word);
-    let b = (await this._similarWord(word + '%'))
-      .filter(w => !a.map(w => w.word).includes(w.word));
-    if (a.length || b.length) {
-      this.response.generateResponse([...a, ...b]);
+
+    if (word.match(/[က-၏]/)) {
+      let result = await this._findBurmese('%' + word + '%');
+      let fuse = new Fuse(result, {
+        keys: ['defination'],
+      });
+      if (result.length) {
+        this.response.generateResponse([
+          ...fuse.search(word).map(({ item }) => item),
+        ]);
+      } else {
+        this.response.generateFallback();
+      }
     } else {
-      this.response.generateFallback();
+      let a = await this._findWord(word);
+      let b = (await this._similarWord(word + '%')).filter(
+        (w) => !a.map((w) => w.word).includes(w.word)
+      );
+      if (a.length || b.length) {
+        this.response.generateResponse([...a, ...b]);
+      } else {
+        this.response.generateFallback();
+      }
     }
   }
 
   isBotCommand(entities) {
     return !!entities.find((v) => v.type == 'bot_command');
   }
-  
-  _findWord(w) {
+
+  _findBurmese(word_input, max_rows = 100) {
     return supabase
-      .post('/rest/v1/rpc/find_word', { w })
+      .post('/rest/v1/rpc/find_burmese', { word_input, max_rows })
       .catch((e) => []);
   }
-  
+
+  _findWord(w) {
+    return supabase.post('/rest/v1/rpc/find_word', { w }).catch((e) => []);
+  }
+
   _similarWord(w, x = 5) {
     return supabase
       .post('/rest/v1/rpc/similar_word', { w, x })
