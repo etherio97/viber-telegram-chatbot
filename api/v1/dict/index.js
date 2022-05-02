43ce595844bf7c3928723fa1e331e28a56a77c37
etherio97 <ethereal97@gmail.com>
2022-05-03 00:34:47 +0630

    api

--- api/v1/dict/index.js
+++ api/v1/dict/index.js
@@ -1,3 +1,60 @@
 import { Router, json } from 'express';
-import supabase from '../../../src/app/supabase';
+import { findBurmese, findWord, similarWord } from '../../../src/app/dict';
+import { sortItems } from '../../../src/utils/helpers';
 
+const router = Router();
+
+router.get('/match/en', async (req, res) => {
+  let { q } = req.query;
+  if (!q) {
+    return res.status(400).json({
+      error: 'required paramter: q',
+    });
+  }
+  let text = q.trim();
+  res.json(
+    sortItems(['word'], await findWord(text), text)
+  );
+});
+
+router.get('/match/mm', async (req, res) => {
+  let { q } = req.query;
+  if (!q) {
+    return res.status(400).json({
+      error: 'required paramter: q',
+    });
+  }
+  let text = q.trim();
+  res.json(
+    sortItems(['definition'], await findBurmese(text, 100), text)
+  );
+});
+
+router.get('/search/en', async (req, res) => {
+  let { q } = req.query;
+  if (!q) {
+    return res.status(400).json({
+      error: 'required paramter: q',
+    });
+  }
+  let text = q.trim();
+  res.json(
+    sortItems(['definition'], await findWord(text + '%'), text)
+  );
+});
+
+router.get('/search/mm', async (req, res) => {
+  let { q } = req.query;
+  if (!q) {
+    return res.status(400).json({
+      error: 'required paramter: q',
+    });
+  }
+  let text = q.trim();
+  res.json(
+    sortItems(['definition'], await findBurmese('%' + text + '%'), text)
+  );
+});
+
+
+export default router;
